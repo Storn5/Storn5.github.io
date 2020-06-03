ba519a945c8994e92db298a59914f292adbcdfa3
Storn5 <storn5@ukr.net>
2020-06-03 15:56:53 +0300

    Updates

---
+++ precache-manifest.1cdbb6164a9e387a1df1e02ba34521d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408916469519896ab0a810704bcd836b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17b821d4958b6d6e3cbd",
+    "url": "/static/css/main.4ee95eb7.chunk.css"
+  },
+  {
+    "revision": "7e45239d050e46c78e29",
+    "url": "/static/js/2.fbee1df2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fbee1df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b821d4958b6d6e3cbd",
+    "url": "/static/js/main.3d916515.chunk.js"
+  },
+  {
+    "revision": "8338c79e3b26593c4b77",
+    "url": "/static/js/runtime-main.6fa46707.js"
+  }
+]);
